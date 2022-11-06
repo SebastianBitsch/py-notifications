a4b11a5c0693e981a01ba64178b2c3e1f4eb61b9
Sebastian Bitsch <sebastianbitsch@protonmail.com>
2022-11-06 18:43:56 +0100

    made body an optional parameter

--- Notifications.py
+++ Notifications.py
@@ -1,22 +1,18 @@
 import os
 
-def notify(text, *args, **kwargs):
+def notify(body:str = "Your code has finished running", title:str = "Task completed", substitle:str = ""):
     """
-    Display a global notification, usefull when waiting for a long block of code to execute.
+    Display a global notification, useful when waiting for a long block of code to execute.
     
     Parameters
     ----------
-    text: str, required
-        The main body of text to be displayed in the notification
+    body: str, optional
+        The main body of body to be displayed in the notification
 
     title: str, optional
         The title of the notification
 
     subtitle: str, optional
-        The subtitle of the notification, written between the title and text
-    """ 
-
-    defaultKwargs = { 'title': "Task completed", 'subtitle': "" }
-    kwargs = { **defaultKwargs, **kwargs }
-    
-    os.system("""osascript -e 'display notification "{}" with title "{}" subtitle "{}" sound name "Submarine"'""".format(text,kwargs['title'],kwargs['subtitle']))
+        The subtitle of the notification, written between the title and body
+    """
+    os.system(f"""osascript -e 'display notification "{body}" with title "{title}" subtitle "{substitle}" sound name "Submarine"'""")
